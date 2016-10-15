8073503456fd726fc7ad13cbba282f7aead6e821
Bojan Stefanovic <bojanstef4@gmail.com>
2016-10-15 13:21:21 -0700

    Simple TCP client server from Python.org

---
+++ client.py
@@ -0,0 +1,21 @@
+import sys
+import socket
+
+address = "localhost", 10008
+data = " ".join(sys.argv[1:])
+
+# Create a socket (SOCK_STREAM means a TCP socket)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    # Connect to server and send data
+    sock.connect(address)
+    sock.sendall(data + "\n")
+
+    # Receive data from the server and shut down
+    received = sock.recv(1024)
+finally:
+    sock.close()
+
+print "Sent:     {}".format(data)
+print "Received: {}".format(received)
